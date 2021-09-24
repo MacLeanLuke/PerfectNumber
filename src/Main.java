8271cb762750e5b7ff0be3842d61146afbecdbaf
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-09-23 19:36:49 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("6 is a perfect number: " + PerfectNumber.isPerfectNumber(6));
+        System.out.println("28 is a perfect number: " + PerfectNumber.isPerfectNumber(28));
+        System.out.println("5 is a perfect number: " + PerfectNumber.isPerfectNumber(5));
+        System.out.println("-1 is a perfect number: " + PerfectNumber.isPerfectNumber(-1));
+    }
+}
---
+++ src/PerfectNumber.java
@@ -0,0 +1,18 @@
+public class PerfectNumber {
+    public static boolean isPerfectNumber(int number){
+        if (number < 1 ){
+            return false;
+        } else {
+            int total = 0;
+            for (int i = 1; i < number; i++){
+                if (number % i == 0){
+                    total += i;
+                }
+            }
+            if (total == number){
+                return true;
+            }
+            return false;
+        }
+    }
+}
